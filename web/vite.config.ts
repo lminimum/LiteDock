fe53094ca1b8b7928aa2070fd872626e02308dff
Iminimum <lgp74217553@gmail.com>
2026-04-29 13:46:49 +0000

    chore(web): add oi.lch.ink to allowedHosts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3023,
       host: '0.0.0.0',
+      allowedHosts: ['oi.lch.ink'],
       proxy: {
         '/v1': {
           target,
